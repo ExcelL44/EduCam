2d6d78efde9a2997dee76c1bfb6e11ea5f0fa23f
ExcelL44 <leonelobion21@icloud.com>
2025-11-27 05:29:57 +0100

    Updating Admin buttonst 2

--- app/src/main/java/com/excell44/educam/ui/screen/admin/LocalDatabaseScreen.kt
+++ app/src/main/java/com/excell44/educam/ui/screen/admin/LocalDatabaseScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.excell44.educam.ui.viewmodel.LocalDatabaseViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
